7fb907d4c6e32414dde7c6b3c1311aefb2998703
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2017-12-01 10:39:53 +0900

    http sample

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ exports.helloWorld = function helloWorld (event, callback) {
   console.log(`My Cloud Function: ${event.data.message}`);
   callback();
 };
+
+exports.httpSample = function httpSample (req, res) {
+  res.status(200).send('Hello from cloud functions');
+};
